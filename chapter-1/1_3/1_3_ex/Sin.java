03dc2895b3045803ed74db167059b4c94e58765e
Danilo Quattrini <daniloquattrini.com@gmail.com>
2025-07-15 12:51:17 +0200

    [fix]: created a better version of the code Sin.java

--- chapter-1/1_3/1_3_ex/Sin.java
+++ chapter-1/1_3/1_3_ex/Sin.java
@@ -1,19 +1,21 @@
+/**<h1>1.3.39 Sin and Cos</h1>
+ * Trigonometric functions. Write two programs, Sin and Cos, that
+ * compute the sine and cosine functions using their Taylor series expansions
+ * **/
 public class Sin {
     public static void main(String[] args) {
         double x = Double.parseDouble(args[0]);
         double sin = 0.0;
         double term = 1.0;
+        int sign = 1; // Positive sign inside the Taylor Sequence
         for (int n = 1; sin != sin + term; n+=2) {
-            double num = 1.0, den = 1.0;
-            for (int i = 1; i <= n; i++)  num *= x;
-            for (int i = 1; i <= n; i++)  den *= i;
-            System.out.print(den + " ");
-            System.out.print(num + " ");
-            term = num / den;
+            term = 1.0;
+            for (int i = 1; i <= n; i++) term *= x / i;
+            term *= sign; // Give the sign to the term variable in base of the position inside the sequence
             sin += term;
-            System.out.println();
+            sign *= -1; // The next sign it would be negative as the rule says
         }
         System.out.println("Math.sin(): " + Math.sin(x));
-        System.out.println("sin x:" + sin);
+        System.out.println("sin x:" +  sin);
     }
 }
